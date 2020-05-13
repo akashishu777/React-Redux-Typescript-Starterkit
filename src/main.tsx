7b65871ca48352a4d677bf2efc1ddb2c14f7c47e
Akash Vishwakarma <akash.vishwakarma@milliman.com>
2020-05-13 10:02:20 +0530

    component folder

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,5 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
+import App from './Components/App';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
