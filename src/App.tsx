eb1e5a9fd6f3460defea6beca53c6920fbdc1770
Akash Vishwakarma <akash.vishwakarma@milliman.com>
2020-05-13 09:59:11 +0530

    Added App Component

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+
+export default class App extends React.PureComponent {
+
+    constructor(props: any) {
+        super(props);
+    }
+
+    
+    render() {
+        return (
+            <p>Hello this is App Component</p>
+        )
+    }
+
+} 
